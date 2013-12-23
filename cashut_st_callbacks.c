bee9dccebc597476e34a29bfae3557581c74c13b
christophe-r <contact@christophe-ribot.fr>
2013-12-23 19:39:21 +0100

    Correction fautes orthographe partie Statistiques

--- cashut_st_callbacks.c
+++ cashut_st_callbacks.c
@@ -21,28 +21,28 @@ void on_notebook_change_page0(){
 
 void st_afficher_stats_ac()
 {
-	int caisse_nombre_vente;
-	float caisse_somme_total;
+	int caisse_nombre_ventes;
+	float caisse_somme_totale;
 	float caisse_somme_montant_moyen;
 
 	gtk_label_set_text(GTK_LABEL(gtk_builder_get_object (builder_cashut, "ac_label_nom_utilisateur")), g_strconcat("Utilisateur : ", nom_utilisateur, NULL));
 
-	caisse_nombre_vente = mysql_nombre_ventes_aujourdhui(num_utilisateur);
+	caisse_nombre_ventes = mysql_nombre_ventes_aujourdhui(num_utilisateur);
 	char ac_caisse_monbre_vente[10];
-	snprintf(ac_caisse_monbre_vente,10,"%d",caisse_nombre_vente);	
-	gtk_label_set_text(GTK_LABEL(gtk_builder_get_object (builder_cashut, "ac_label_total_vente")), ac_caisse_monbre_vente);
+	snprintf(ac_caisse_monbre_vente, 10, "%d", caisse_nombre_ventes);	
+	gtk_label_set_text(GTK_LABEL(gtk_builder_get_object (builder_cashut, "ac_label_total_ventes")), ac_caisse_monbre_vente);
 	
-	caisse_somme_total = mysql_total_encaisse_aujourdhui(num_utilisateur);
-	char ac_caisse_somme_total[10];
-	snprintf(ac_caisse_somme_total,10,"%.2f",caisse_somme_total);
-	gtk_label_set_text(GTK_LABEL(gtk_builder_get_object (builder_cashut, "ac_label_somme_total")), g_strconcat(ac_caisse_somme_total," €",NULL));
+	caisse_somme_totale = mysql_total_encaisse_aujourdhui(num_utilisateur);
+	char ac_caisse_somme_totale[10];
+	snprintf(ac_caisse_somme_totale, 10, "%.2f", caisse_somme_totale);
+	gtk_label_set_text(GTK_LABEL(gtk_builder_get_object (builder_cashut, "ac_label_somme_totale")), g_strconcat(ac_caisse_somme_totale, " €", NULL));
 
 
-	if( caisse_nombre_vente > 0 ){
-		caisse_somme_montant_moyen = caisse_somme_total / caisse_nombre_vente;
+	if( caisse_nombre_ventes > 0 ){
+		caisse_somme_montant_moyen = caisse_somme_totale / caisse_nombre_ventes;
 		char ac_caisse_somme_montant_moyen[10];
-		snprintf(ac_caisse_somme_montant_moyen,10,"%.2f",caisse_somme_montant_moyen);
-		gtk_label_set_text(GTK_LABEL(gtk_builder_get_object (builder_cashut, "ac_label_montant_moyen")), g_strconcat(ac_caisse_somme_montant_moyen," €",NULL));
+		snprintf(ac_caisse_somme_montant_moyen, 10, "%.2f", caisse_somme_montant_moyen);
+		gtk_label_set_text(GTK_LABEL(gtk_builder_get_object (builder_cashut, "ac_label_montant_moyen")), g_strconcat(ac_caisse_somme_montant_moyen, " €", NULL));
 	} else {
 		gtk_label_set_text(GTK_LABEL(gtk_builder_get_object (builder_cashut, "ac_label_montant_moyen")), "0.00 €");
 	}
@@ -56,35 +56,35 @@ void on_notebook_change_page4(){
 
 void st_afficher_stats_stats()
 {
-	int magasin_nombre_vente;
-	int nombre_produit;
+	int magasin_nombre_ventes;
+	int nombre_produits;
 	float magasin_somme_totale;
 	float magasin_montant_moyen;
 
-	magasin_nombre_vente = mysql_nombre_ventes_aujourdhui("0");
+	magasin_nombre_ventes = mysql_nombre_ventes_aujourdhui("0");
 	char st_magasin_monbre_vente[10];
-	snprintf(st_magasin_monbre_vente,10,"%d",magasin_nombre_vente);		
-	gtk_label_set_text(GTK_LABEL(gtk_builder_get_object (builder_cashut, "st_label_magasin_total_vente")), st_magasin_monbre_vente);
+	snprintf(st_magasin_monbre_vente, 10, "%d", magasin_nombre_ventes);		
+	gtk_label_set_text(GTK_LABEL(gtk_builder_get_object (builder_cashut, "st_label_magasin_total_ventes")), st_magasin_monbre_vente);
 
 	magasin_somme_totale = mysql_total_encaisse_aujourdhui("0");
 	char st_magasin_somme_totale[10];
-	snprintf(st_magasin_somme_totale,10,"%.2f",magasin_somme_totale);
-	gtk_label_set_text(GTK_LABEL(gtk_builder_get_object (builder_cashut, "st_label_magasin_somme_total")), g_strconcat(st_magasin_somme_totale," €",NULL));
+	snprintf(st_magasin_somme_totale, 10, "%.2f", magasin_somme_totale);
+	gtk_label_set_text(GTK_LABEL(gtk_builder_get_object (builder_cashut, "st_label_magasin_somme_totale")), g_strconcat(st_magasin_somme_totale, " €", NULL));
 
 
-	if( magasin_nombre_vente > 0 ){
-		magasin_montant_moyen = magasin_somme_totale / magasin_nombre_vente;
+	if( magasin_nombre_ventes > 0 ){
+		magasin_montant_moyen = magasin_somme_totale / magasin_nombre_ventes;
 		char st_magasin_montant_moyen[10];
-		snprintf(st_magasin_montant_moyen,10,"%.2f",magasin_montant_moyen);
-		gtk_label_set_text(GTK_LABEL(gtk_builder_get_object (builder_cashut, "st_label_magasin_montant_moyen")), g_strconcat(st_magasin_montant_moyen," €",NULL));
+		snprintf(st_magasin_montant_moyen, 10, "%.2f", magasin_montant_moyen);
+		gtk_label_set_text(GTK_LABEL(gtk_builder_get_object (builder_cashut, "st_label_magasin_montant_moyen")), g_strconcat(st_magasin_montant_moyen, " €", NULL));
 	} else {
 		gtk_label_set_text(GTK_LABEL(gtk_builder_get_object (builder_cashut, "st_label_magasin_montant_moyen")), "0.00 €");
 	}
 
-	nombre_produit = mysql_nombre_produits();
-	char st_magasin_nombre_produit[10];
-	snprintf(st_magasin_nombre_produit,10,"%d",nombre_produit);	
-	gtk_label_set_text(GTK_LABEL(gtk_builder_get_object (builder_cashut, "st_label_magasin_nb_produit")), st_magasin_nombre_produit);
+	nombre_produits = mysql_nombre_produits();
+	char st_magasin_nombre_produits[10];
+	snprintf(st_magasin_nombre_produits, 10, "%d", nombre_produits);	
+	gtk_label_set_text(GTK_LABEL(gtk_builder_get_object (builder_cashut, "st_label_magasin_nb_produits")), st_magasin_nombre_produits);
 	
 	gtk_label_set_text(GTK_LABEL(gtk_builder_get_object (builder_cashut, "st_label_magasin_dernier_encaissement")), mysql_derniere_vente());
 	
